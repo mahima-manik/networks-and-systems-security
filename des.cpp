9d7d4f780154467f4f283bcf3245fdd7290ddd18
srutigoyal <35297766+srutigoyal@users.noreply.github.com>
2018-01-23 10:06:37 +0530

    Add files via upload

--- des.cpp
+++ des.cpp
@@ -56,6 +56,39 @@ int st_p_box[4][8]={{16, 7, 20, 21, 29, 12, 28, 17},
                     {1, 15, 23, 26, 5, 18, 31, 10},
                     {2, 8, 24, 14, 32, 27, 3, 9},
                     {19, 13, 30, 6, 22, 11, 4, 25}};
+
+int ip[8][8]={{58, 50, 42, 34, 26, 18, 10, 2},
+            {60, 52, 44, 36, 28, 20, 12, 4},
+            {62, 54, 46, 38, 30, 22, 14, 6},
+            {64, 56, 48, 40, 32, 24, 16, 8},
+            {57, 49, 41, 33, 25, 17, 9, 1},
+            {59, 51, 43, 35, 27, 19, 11, 3},
+            {61, 53, 45, 37, 29, 21, 13, 5},
+            {63, 55, 47, 39, 31, 23, 15, 7}};
+int fp[8][8]={{40, 8, 48, 16, 56, 24, 64, 32},
+                {39, 7, 47, 15, 55, 23, 63, 31},
+                {38, 6, 46, 14, 54, 22, 62, 30},
+                {37, 5, 45, 13, 53, 21, 61, 29},
+                {36, 4, 44, 12, 52, 20, 60, 28},
+                {35, 3, 43, 11, 51, 19, 59, 27},
+                {34, 2, 42, 10, 50, 18, 58, 26},
+                {33, 1, 41, 9, 49, 17, 57, 25}};
+int* initial_permutation(int* plaintext)
+{
+    int* pt=new int[64];
+    for(int i=0;i<8;i++)
+        for(int j=0;j<8;j++)
+            pt[i*8+j]=plaintext[ip[i][j]-1];
+    return pt;
+}
+int* final_permutation(int* plaintext)
+{
+    int* pt=new int[64];
+    for(int i=0;i<8;i++)
+        for(int j=0;j<8;j++)
+            pt[i*8+j]=plaintext[fp[i][j]-1];
+    return pt;
+}
 //applying expansion p box
 int* expansion_p_box(int* plaintext)
 {
@@ -63,6 +96,10 @@ int* expansion_p_box(int* plaintext)
     for(int i=0;i<8;i++)
         for(int j=0;j<6;j++)
             pt[i*6+j]=plaintext[ex_p_box[i][j]-1];
+    cout<<"In expansion p box"<<endl;
+    for(int i=0;i<48;i++)
+        cout<<pt[i];
+    cout<<endl;
     return pt;
 }
 //xor operation of n bits
@@ -70,7 +107,11 @@ int* xor_op(int* a, int* b, int n)
 {
     int *p=new int[n];
     for(int i=0;i<n;i++)
+    {
       p[i]=a[i] xor b[i];
+      //cout<<p[i]<<" "<<a[i]<<" "<<b[i]<<endl;
+    }
+
     return p;
 }
 //convert 2 bit binary to decimal
@@ -128,6 +169,10 @@ int* func(int* pt, int* key)
     cout<<"Expansion p box applied"<<endl;
     for(int i=0;i<48;i++)
         cout<<plaintext[i];
+    cout<<"key before xor"<<endl;
+    for(int i=0;i<48;i++)
+        cout<<key[i];
+    cout<<endl;
     plaintext=xor_op(plaintext, key, 48);
     cout<<"xor applied"<<endl;
     for(int i=0;i<48;i++)
@@ -149,6 +194,13 @@ int* round(int* l, int *r, int* key)
 {
     r=func(r, key);
     cout<<"func applied"<<endl;
+    cout<<"l r before xor"<<endl;
+    for(int i=0;i<32;i++)
+        cout<<l[i];
+    cout<<endl;
+    for(int i=0;i<32;i++)
+        cout<<r[i];
+    cout<<endl;
     l=xor_op(l, r, 32);
     cout<<"xor applied"<<endl;
         for(int i=0;i<32;i++)
@@ -200,7 +252,7 @@ int* paritydrop(int* cipherkey)  {    //converts the 64 bits to 56 bits by dropp
 }
 int * encrypt(int* plaintext, int *key)
 {
-//    plaintext=initial_permutation(plaintext);
+    plaintext=initial_permutation(plaintext);
     for(int j=0;j<16;j++)
     {
         int* l = new int[32];
@@ -214,10 +266,15 @@ int * encrypt(int* plaintext, int *key)
             p[i]=plaintext[i+32];
         }
         cout<<endl;
+        cout<<"plaintext before round"<<endl;
+        for(int i=0;i<64;i++)
+            cout<<plaintext[i];
+        cout<<endl;
         l=round(l, r, key);
         for(int i=0;i<32;i++)
             p[i+32]=l[i];
-        plaintext=p;
+        for(int i=0;i<64;i++)
+            plaintext[i]=p[i];
         cout<<"ciphertext of round "<<j<<endl;
         for(int i=0;i<64;i++)
             cout<<plaintext[i];
@@ -226,12 +283,12 @@ int * encrypt(int* plaintext, int *key)
         delete[] r;
         delete[] p;
     }
-//    plaintext=final_permutation(plaintext);
+    plaintext=final_permutation(plaintext);
     return plaintext;
 }
 int * decrypt(int* plaintext, int *key)
 {
-//    plaintext=initial_permutation(plaintext);
+    plaintext=initial_permutation(plaintext);
     for(int j=0;j<16;j++)
     {
         int* l = new int[32];
@@ -248,11 +305,14 @@ int * decrypt(int* plaintext, int *key)
         for(int i=0;i<32;i++)
             p[i]=r[i];
         plaintext=p;
+        cout<<"decrypt round "<<j<<endl;
+        for(int i=0;i<64;i++)
+            cout<<plaintext[i];
         delete[] l;
         delete[] r;
         delete[] p;
     }
-//    plaintext=final_permutation(plaintext);
+    plaintext=final_permutation(plaintext);
     return plaintext;
 
 }
@@ -295,7 +355,18 @@ int main()  {
   for(int i=0; i<48;i++)
     cout<<k[i];
   cout<<endl;;
-
+  /*int *ct=new int [64];
+  ct=initial_permutation(pt);
+  cout<<"Initial permutation:"<<endl;
+  for(int i=0; i<64;i++)
+    cout<<ct[i];
+  cout<<endl;
+  int *d=new int [64];
+  d=final_permutation(ct);
+  cout<<"Final permutation:"<<endl;
+  for(int i=0; i<64;i++)
+    cout<<d[i];
+  cout<<endl;*/
   int *ct=new int [64];
   ct=encrypt(pt, k);
   cout<<"Ciphertext after 1 round:"<<endl;
